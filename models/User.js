687771f21e88c37f49df8ba23f05f05047bddda3
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-10-24 20:49:00 -0500

    created file structure and set up user model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+//username
+//email
+//thoughts
+//friends
+//virtual -- friend counts
+const userSchema = new Schema (
+    {
+        username: { type: String, unique: true, required: true, trim: true },
+        email: { 
+            type: String,
+            required: true,
+            match: /.+\@.+\..+/,
+            unique: true },
+        thoughts: [
+            {type: Schema.Types.ObjectId,
+            ref: 'thought'}
+        ],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+)
+
+// userSchema
+//   .virtual('friendcount')
+//   // Getter
+//   .get(function () {
+//     return this.friends;
+//   })
+//   // Setter to set the first and last name
+//   .set(function (fc) {
+//     const 
+//     this.set({ });
+//   });
+
+const User = model('user', userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Reaction = require('./Reaction');
+const Thought = require('./Though');
+
+module.exports = { User, Thought, Reaction };
